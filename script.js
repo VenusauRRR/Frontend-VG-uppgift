d5368847aa233e28534efd3e52efc4291ea8b7e7
Venus Poon <venuspsw@hotmail.com>
2024-03-28 23:53:51 +0100

    add amount increment & decrement buttons

--- script.js
+++ script.js
@@ -65,3 +65,84 @@ function displayProducts(categoryTitle,categoryAPI) {
              });
          });
  }
+
+
+ let selectedProductList = [];
+
+ function sendData(product) {
+    console.log(product);
+    selectedProductList.push(product);
+    localStorage.setItem("selectedProductList", JSON.stringify(selectedProductList));
+
+}
+
+
+function displayProductInfo() {
+    const product = JSON.parse(localStorage.getItem("selectedProductList"));
+    console.log(product);
+
+    const productInfo = document.getElementById("productList");
+    product.forEach(element => {
+        const div = document.createElement('div');
+        div.classList.add('row');
+        div.classList.add('border-bottom');
+        div.classList.add('border-success');
+        div.classList.add('p-2');
+        div.classList.add('d-flex');
+        div.classList.add('align-items-center');
+
+        const prodImg = document.createElement('img');
+    prodImg.src = element.image;
+    prodImg.alt = element.title;
+    // prodImg.classList.add('card-img-top');
+    // prodImg.classList.add('bg-secondary');
+    prodImg.style.maxHeight = '10em';
+    prodImg.style.maxWidth = '10em';
+    prodImg.classList.add('col-md-2');
+
+    div.appendChild(prodImg);
+
+    let amountBtnDiv = document.createElement('div');
+    amountBtnDiv.innerHTML = `<span class="minus">-</span>
+    <span class="num">01</span>
+    <span class="plus">+</span>
+    `;
+    amountBtnDiv.classList.add('col-md-1');
+    amountBtnDiv.style.cursor = 'pointer';
+
+
+    div.innerHTML += `
+    <div class="col-md-8">${element.title}</div>
+    <div class="col-md-1">$${element.price}</div>
+    `;
+
+    div.appendChild(amountBtnDiv);
+
+    productInfo.appendChild(div);
+
+    const minus = document.querySelector('.minus');
+const plus = document.querySelector('.plus');
+const num = document.querySelector('.num');
+
+let amountNum = 1;
+plus.addEventListener('click',() => {
+    amountNum++;
+    num.innerHTML = amountNum < 10? '0'+amountNum : amountNum;
+})
+
+minus.addEventListener('click',() => {
+    amountNum--;
+    if (amountNum < 1){
+        num.innerHTML = 0;
+    } else if (amountNum >0 && amountNum < 10){
+        num.innerHTML = '0'+amountNum;
+    } else {
+        num.innerHTML = amountNum;
+    }
+})
+
+    });
+}
+
+            
+
